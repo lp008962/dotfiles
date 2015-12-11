81170a816057956bca9fe1a958712218348a2223
Kuroi <lp008962@hotmail.com>
2015-12-11 16:35:14 +0900

    first commit

---
+++ install_dotfiles.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+path=$HOME/dotfiles
+dotfiles=('.vimrc' '.zshrc' '.tmux.conf')
+
+chdir /tmp
+wget https://github.com/lp008962/dotfiles/archive/master.tar.gz
+tar zxvf master.tar.gz
+mv dotfiles-master $path
+
+for f in ${dotfiles[@]}; do
+  ln -s $path/$f $HOME/$f
+done
+
+
