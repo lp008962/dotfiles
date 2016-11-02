7f3ff32fa62e1eab02feb31a90c898c63b93e6a1
hoshino <lp008962@hotmail.com>
2016-11-02 14:58:49 +0900

    Change installing from wget to git

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+sudo yum install -y git tmux zsh
+chsh
+
 path=$HOME/dotfiles
 dotfiles=('.vimrc' '.zshrc' '.tmux.conf')
 
-chdir /tmp
-wget https://github.com/lp008962/dotfiles/archive/master.tar.gz
-tar zxvf master.tar.gz
-mv dotfiles-master $path
+#chdir /tmp
+#wget https://github.com/lp008962/dotfiles/archive/master.tar.gz
+#tar zxvf master.tar.gz
+#mv dotfiles-master $path
+cd $path
+git clone https://github.com/lp008962/dotfiles
 
 for f in ${dotfiles[@]}; do
   ln -s $path/$f $HOME/$f
 done
 
 git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/Vundle.vim
-
-
