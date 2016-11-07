4e2378279770e5c1e40c314c95780fa6897427d3
Kuroi <lp008962@hotmail.com>
2016-11-07 16:18:33 +0900

    Add peco installation

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -5,10 +5,12 @@ chsh
 path=$HOME/dotfiles
 dotfiles=('.vimrc' '.zshrc' '.tmux.conf')
 
+## Using wget if git cannot install
 #chdir /tmp
 #wget https://github.com/lp008962/dotfiles/archive/master.tar.gz
 #tar zxvf master.tar.gz
 #mv dotfiles-master $path
+
 cd $path
 git clone https://github.com/lp008962/dotfiles
 
@@ -16,4 +18,23 @@ for f in ${dotfiles[@]}; do
   ln -s $path/$f $HOME/$f
 done
 
+
+# Installing vim-Vundle
 git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/Vundle.vim
+
+
+# Instaling peco
+URL='https://github.com/peco/peco/releases/download/'
+VER=`./get_peco_latest_version.pl`
+PECO='peco_linux_amd64'
+
+cd /tmp
+wget ${URL}${VER}/${PECO}.tar.gz
+tar -zxf $PECO
+
+if [ -e $HOME/bin ]; then
+    cp $PECO/peco $HOME/bin/
+else
+    mkdir $HOME/bin
+    cp $PECO/peco $HOME/bin/
+fi
