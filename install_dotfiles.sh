79f368e77d44a8599b4d37cdb6c462dac705f773
Kuroi <lp008962@hotmail.com>
2016-11-21 13:32:27 +0900

    fix getting latest-peco

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -5,7 +5,7 @@ chsh
 path=$HOME/dotfiles
 dotfiles=('.vimrc' '.zshrc' '.tmux.conf')
 
-## Using wget if git cannot install
+#### Using wget if git cannot install
 #chdir /tmp
 #wget https://github.com/lp008962/dotfiles/archive/master.tar.gz
 #tar zxvf master.tar.gz
@@ -19,17 +19,16 @@ for f in ${dotfiles[@]}; do
 done
 
 
-# Installing vim-Vundle
+### Installing vim-Vundle
 git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/Vundle.vim
 
 
-# Instaling peco
-URL='https://github.com/peco/peco/releases/download/'
-VER=`./get_peco_latest_version.pl`
-PECO='peco_linux_amd64'
-
+### Instaling peco
 cd /tmp
-wget ${URL}${VER}/${PECO}.tar.gz
+URL='https://github.com/peco/peco/releases'
+PECO='peco_linux_amd64'
+VER=`curl -sI ${URL}/latest | awk -F'/' '/^Location:/{print $NF}' | tr -d ""`
+wget - ${URL}/download/${VER}/${PECO}.tar.gz
 tar -zxf $PECO.tar.gz
 
 if [ -e $HOME/bin ]; then
